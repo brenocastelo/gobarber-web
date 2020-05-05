5cab1cd01c76844840a8eee65a09bccf8179f6d0
Breno Castelo Branco <breno@vizir.com.br>
2020-05-05 20:34:52 -0300

    feat: config private routes

---
+++ src/routes/Route.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const isSigned = true;
+
+  if (!isSigned && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (isSigned && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
