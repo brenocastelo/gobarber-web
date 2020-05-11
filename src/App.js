0c4ccbe84dda54aa7e4da1c05ccd877086798f6b
Breno Castleo Branco <breno.cbsousa@gmail.com>
2020-05-11 20:57:01 -0300

    feat: configure redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './configs/reactotronConfig';
+
+import store from './store';
 
 import History from './services/history';
 import Routes from './routes';
@@ -10,10 +13,12 @@ import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={History}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={History}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
