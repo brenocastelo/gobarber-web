7a61ea9d029d85f2dededcf46959a13c4b8a16d4
Breno Castleo Branco <breno.cbsousa@gmail.com>
2020-05-12 19:59:40 -0300

    feat: authentication with redux

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,14 @@ import PropTypes from 'prop-types';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const isSigned = false;
+  const { isSigned } = store.getState().auth;
 
   if (!isSigned && isPrivate) {
     return <Redirect to="/" />;
