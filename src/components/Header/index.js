fcc0d39598282035d52f43e5fde698c1b9c0f476
Breno Castleo Branco <breno.cbsousa@gmail.com>
2020-05-18 20:01:33 -0300

    feat: create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+import logo from '../../assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Go Barber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Breno Castelo Branco</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="profile"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #eee;
+    }
+
+    a {
+      font-weight: bold;
+      color: #7159c1;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #333;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+
+  img {
+    height: 32px;
+    border-radius: 50%;
+  }
+`;
